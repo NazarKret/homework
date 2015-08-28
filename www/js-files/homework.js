dcfdff28f124a0db07ade6335807414a69c0d9cc
nazarkret <kret@vikk.com.ua>
2015-08-28 19:52:04 +0000

    Created index

---
+++ www/js-files/homework.js
@@ -0,0 +1,29 @@
+$(function(){
+//************************************** DROPDOWN ****************************************\\
+$("li").has(".dropdown").hover(
+	function(){
+		$(this).find(".dropdown").slideDown();	
+	},
+	
+	function(){
+		$(this).find(".dropdown").slideUp();	
+	}
+);
+
+//************************************** THUMBNAILS ***************************************\\
+$(".thumbnails li img").hover(
+	function(){
+		$(this).animate({
+			width: "90px",
+			height: "90px"
+		}, 500);	
+	},
+	function(){
+		$(this).animate({
+			width: "50px",
+			height: "50px"
+		}, 500);	
+	}
+);
+
+});
